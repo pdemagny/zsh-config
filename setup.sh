7c4e4313bccaf22bb0ed3ec47a69fa08659e6018
Pierre DEMAGNY <pierre.demagny@citymeo.fr>
2019-05-30 11:54:02 +0200

    adding aliases.txt file used by the zal aliases to grep in git & debian aliases

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 mkdir -p ~/.local/share/fonts
 (cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf)
-cp -v ./.zsh.aliases ./.zsh.comp ./.zsh.env .zshrc ~
+cp -v ./.zsh.aliases ./.zsh.aliases.txt ./.zsh.comp ./.zsh.env .zshrc ~
